e6117531bbb01c42eb0a4b5c4a9ddbd34da239f3
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-30 13:51:29 -0500

    comments added to indexEvents.

---
+++ public/assets/js/indexEvents.js
@@ -0,0 +1,5 @@
+// create funcitons to handle the saving of drawings
+
+// craete funcitons to create list items for each drawing that a user has
+
+// create functions to handle the deletion of a drawing
