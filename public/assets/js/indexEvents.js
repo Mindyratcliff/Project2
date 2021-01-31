ba45b90e89eccd8a701c47bcc1d7651c0f3a0f55
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-31 10:19:06 -0500

    Additional comments added.

--- public/assets/js/indexEvents.js
+++ public/assets/js/indexEvents.js
@@ -1,3 +1,7 @@
+// bring things in if they need to be brought in
+
+// create variables to hold html elements
+
 // create funcitons to handle the saving of drawings
 
 // craete funcitons to create list items for each drawing that a user has
