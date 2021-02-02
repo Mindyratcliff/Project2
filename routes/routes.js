20af38a92728846d667826603c3986bee667a06e
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-02-02 03:44:58 -0500

    not working, but added some routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,54 +1,42 @@
 const db = require('../models');
-const express = require('express');
 
-const router = express.Router();
-
-// user authentication route
-router.get('/dashboard', async (req, res) => {
-    console.log(req.user);
-    const drawings = await db.Drawing.findAll({
-        where: { UserId: req.user.dataValues.id },
-        include: [db.User],
+module.exports = (app) => {
+    // user authentication route
+    app.get('/dashboard', async (req, res) => {
+        console.log(req.user);
+        const drawings = await db.Drawing.findAll({
+            where: { UserId: req.user.dataValues.id },
+            include: [db.User],
+        });
+        console.log(drawings);
+        res.send(req.user);
     });
-    console.log(drawings);
-    res.send(req.user);
-});
 
-// get drawings route
-router.post('api/drawings', (req, res) => {
-    drawing.create({
-        author: req.body.title,
-        body: req.body.body,
-        createdAt: req.body.created_at
-    }).then(data => {
-        res.json(data);
+    // get drawings route
+    app.post('api/drawings', async (req, res) => {
+        const newDrawing = await db.Drawing.create({
+            author: req.body.title,
+            body: req.body.body,
+            createdAt: req.body.created_at
+        });
+        res.json(newDrawing);
         res.end();
     });
 
-});
-
-// finds all drawings
-router.get('api/drawings', (req, res) => {
-    drawing.findAll({}).then(data => {
-        res.json(data);
+    // updates a drawing
+    app.put('api/drawings/:id', (req, res) => {
+        db.Drawing.update(req.body, {
+            where: {
+                id: req.body.id,
+            },
+        }).then(data => res.json(data));
     });
-});
 
-// updates a drawing
-router.put('api/drawings/:id', (req, res) => {
-    drawings.update(req.body, {
-        where: {
-            id: req.body.id,
-        },
-    }).then(thisDrawing => res.json(thisDrawing));
-});
-
-router.delete('api/drawings/:id', (req, res) => {
-    drawings.destroy({
-        where: {
-            id: req.params.id,
-        }
-    }).then(thisDrawing => res.json(thisDrawing));
-});
-
-module.exports = router;
+    app.delete('api/drawings/:id', (req, res) => {
+        db.Drawings.destroy({
+            where: {
+                id: req.params.id,
+            }
+        }).then(data => res.json(data));
+    });
+};
