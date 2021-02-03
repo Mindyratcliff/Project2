e0a5143779b79dea71b19a9b1e9049095cdc67e7
Kole Gasior <jkole822@gmail.com>
2021-02-03 17:05:51 -0500

    add try/catch error handling

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,31 +4,54 @@ const requireLogin = require('./../middleware/requireLogin');
 module.exports = app => {
     // fetch the drawings
     app.get('/api/drawings', requireLogin, async (req, res) => {
-        const drawings = await db.Drawing.findAll({
-            where: { UserId: req.user.dataValues.id },
-            include: [db.User],
-        });
-        res.json(drawings);
+        try {
+            const drawings = await db.Drawing.findAll({
+                where: { UserId: req.user.dataValues.id },
+                include: [db.User],
+            });
+            res.json(drawings);
+        } catch (e) {
+            res.json(e);
+        }
+    });
+
+    app.get('/api/drawings/:id', requireLogin, async (req, res) => {
+        try {
+            const drawing = await db.Drawing.findOne({
+                where: { id: req.params.id },
+            });
+            res.json(drawing);
+        } catch (e) {
+            res.json(e);
+        }
     });
 
     // create drawings
     app.post('/api/drawings', requireLogin, async (req, res) => {
-        const newDrawing = await db.Drawing.create({
-            UserId: req.user.dataValues.id,
-            title: req.body.title,
-            body: req.body.body,
-        });
-        res.json(newDrawing);
+        try {
+            const newDrawing = await db.Drawing.create({
+                UserId: req.user.dataValues.id,
+                title: req.body.title,
+                body: req.body.body,
+            });
+            res.json(newDrawing);
+        } catch (e) {
+            res.json(e);
+        }
     });
 
     // updates a drawing
     app.put('/api/drawings/:id', requireLogin, async (req, res) => {
-        const drawing = await db.Drawing.update(req.body, {
-            where: {
-                id: req.body.id,
-            },
-        });
-        res.json(drawing);
+        try {
+            const drawing = await db.Drawing.update(req.body, {
+                where: {
+                    id: req.params.id,
+                },
+            });
+            res.json(drawing);
+        } catch (e) {
+            res.json(e);
+        }
     });
 
     //delete a drawing
@@ -36,7 +59,7 @@ module.exports = app => {
         const drawing = db.Drawings.destroy({
             where: {
                 id: req.params.id,
-            }
+            },
         });
         res.json(drawing);
     });
