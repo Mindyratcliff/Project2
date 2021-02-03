918e6c2ae9fd6f0a064bfd87a3ae2ed5e62c50b4
sziccardi1998 <sziccardifullstack@gmail.com>
2021-02-02 19:42:49 -0500

    comments made to routes.js.

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,20 @@
 const db = require("../models");
 
 module.exports = app => {
+	// check users id and find all drawings that are associated
 	app.get("/dashboard", async (req, res) => {
 		console.log(req.user);
 		const drawings = await db.Drawing.findAll({
 			where: { UserId: req.user.dataValues.id },
 			include: [db.User],
 		});
+		// **STILL TO DO** send drawings to html page
 		console.log(drawings);
 		res.send(req.user);
+	});
+
+	// when drawing is clicked on populate to canvas
+	app.get("/dashboard/:id", async (req, res) => {
+
 	});
 };
