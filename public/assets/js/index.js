346563503776f7484cff05e884aebf64b6b79432
Kole Gasior <jkole822@gmail.com>
2021-02-03 17:13:56 -0500

    remove code for test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,4 @@
 // creating and naming new list items.
-$('#drawing-list').click(() => {
-    $('#drawing-list').append('<div>Fire</div>');
-});
 $('#saveButton').click(() => {
     const title = prompt('What is the file\'s title?').trim();
     const body = window._json;
