03446864f484a5ce702ba69278dc52b3f7a2adcc
Kole Gasior <jkole822@gmail.com>
2021-02-05 14:37:24 -0500

    Add edit and clear functionality

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,22 +1,46 @@
+$(document).ready(() => sessionStorage.setItem('edit', false));
 // creating and naming new list items.
 $('#saveButton').click(() => {
+    const clearCanvas = new Event('clear-canvas');
     const title = prompt('What is the file\'s title?').trim();
     const body = window._json;
 
-    if (title) {
+    const editMode = sessionStorage.getItem('edit');
+
+    if (editMode !== 'true') {
         $.post('/api/drawings', { title, body }).then(data => {
             const $li = $('<li>')
                 .addClass('list-group-item')
                 .attr('data-id', data.id);
-            const $span = $('<span>').text(data.title);
+            const $span = $('<span>').text(title);
             const $i = $('<i>')
                 .addClass('fas fa-trash-alt float-right text-danger delete-note')
                 .attr('data-id', data.id);
 
             $li.append($span).append($i);
             $('#drawing-list').append($li);
+
+            document.dispatchEvent(clearCanvas);
+        });
+    } else if (editMode) {
+        const id = sessionStorage.getItem('current-drawing');
+
+        $.ajax({
+            url: `/api/drawings/${id}`,
+            data: { title, body },
+            type: 'PUT',
+        }).then(() => {
+            $(`[data-id=${id}]`).children('span').text(title);
+            sessionStorage.setItem('edit', false);
+            document.dispatchEvent(clearCanvas);
         });
     }
+});
+
+$('#clearButton').click(() => {
+    const clearCanvas = new Event('clear-canvas');
+    document.dispatchEvent(clearCanvas);
+    sessionStorage.setItem('edit', false);
 });
 
 //deleting list item
@@ -28,6 +52,6 @@ $(document).on('click', '.delete-note', function () {
     $.ajax({
         url: '/api/drawings/' + drawing,
         type: 'DELETE',
-    }).then(console.log(drawing+ ' deleted'));
+    }).then(console.log(drawing + ' deleted'));
 });
 // var dataURL = canvas.toDataURL(); // this code will be used to save canvas as an image (png file)
