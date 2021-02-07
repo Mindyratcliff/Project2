2b0d0631effa6825b5b4b0d0b790fb0a1e40b2c1
Kole Gasior <jkole822@gmail.com>
2021-02-07 14:05:29 -0500

    fix typos

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    googleClientID: process.env.FACEBOOK_CLIENT_ID,
-    googleClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    googleClientID: process.env.GITHUB_CLIENT_ID,
-    googleClientSecret: process.env.GITHUB_CLIENT_SECRET,
+    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    githubClientID: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
 };
