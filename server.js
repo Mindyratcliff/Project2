6f56b6c53c56879b04f123882626c37dffd981bf
Kole Gasior <jkole822@gmail.com>
2021-02-07 14:11:43 -0500

    apply eslint fix

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const passport = require("passport");
-const cookieSession = require("cookie-session");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const exphbs = require('express-handlebars');
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
-const keys = require("./config/keys");
+const keys = require('./config/keys');
 
 // set up express app to use handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Requiring our models for syncing
-const db = require("./models");
-require("./services/passport/google");
-require("./services/passport/github");
-require("./services/passport/facebook");
+const db = require('./models');
+require('./services/passport/google');
+require('./services/passport/github');
+require('./services/passport/facebook');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
-	cookieSession({
-		keys: [keys.cookieKey],
-		maxAge: 1000 * 60 * 60 * 24,
-	})
+    cookieSession({
+        keys: [keys.cookieKey],
+        maxAge: 1000 * 60 * 60 * 24,
+    })
 );
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/auth-routes.js")(app);
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+require('./routes/auth-routes.js')(app);
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(() => {
-	app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
